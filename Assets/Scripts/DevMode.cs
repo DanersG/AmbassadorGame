7fbd55f8b6eeb04c8611c32f9ca0b6df1d6ebfa8
nbradleyCodes <112526708+nbradleyCodes@users.noreply.github.com>
2022-10-11 20:26:12 -0500

    Scene Jumping adding to player
    Hopefully this doesn't mess anything up

---
+++ Assets/Scripts/DevMode.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.SceneManagement;
+public class DevMode: MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space)){
+            SceneManager.LoadScene("DevRoom");
+            //Debug.Log("Go to DevRoom");
+        }
+        if(Input.GetKeyDown(KeyCode.Alpha0)){
+            SceneManager.LoadScene("Beach");
+        }
+        if(Input.GetKeyDown(KeyCode.Alpha1)){
+            SceneManager.LoadScene("Castle_Exterior");
+        }
+        if(Input.GetKeyDown(KeyCode.Alpha2)){
+            SceneManager.LoadScene("Castle_Interior");
+        }
+        if(Input.GetKeyDown(KeyCode.Alpha3)){
+            SceneManager.LoadScene("Church_exterior");
+        }
+        if(Input.GetKeyDown(KeyCode.Alpha4)){
+            SceneManager.LoadScene("Church_Interior");
+        }
+        if(Input.GetKeyDown(KeyCode.Alpha5)){
+            SceneManager.LoadScene("Farm");
+        }
+        if(Input.GetKeyDown(KeyCode.Alpha6)){
+            SceneManager.LoadScene("Forest");
+        }
+        if(Input.GetKeyDown(KeyCode.Alpha7)){
+            SceneManager.LoadScene("Mine_Interior");
+        }
+        if(Input.GetKeyDown(KeyCode.Alpha8)){
+            SceneManager.LoadScene("Prison");
+        }
+        if(Input.GetKeyDown(KeyCode.Alpha9)){
+            SceneManager.LoadScene("Wizard_Exterior");
+        }
+        if(Input.GetKeyDown(KeyCode.Tilde)){
+            SceneManager.LoadScene("Wizard_Interior");
+        }
+    }
+}
