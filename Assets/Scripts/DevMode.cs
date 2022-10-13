2cbb94776a4dc7f67a213caffaa5f8a398bca017
Carson French <tfrench2@harding.edu>
2022-10-13 01:25:38 -0500

    Added spawn points (Delete me if not spawning at 0,0))

--- Assets/Scripts/DevMode.cs
+++ Assets/Scripts/DevMode.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+
 public class DevMode: MonoBehaviour
 {
     // Start is called before the first frame update
     public Text levelLabel;
+    public Vector2 targetPosition;
     private bool Active = false;
+    public Transform player;
 
     // Update is called once per frame
     void Awake()
@@ -31,55 +34,66 @@ public class DevMode: MonoBehaviour
         {
             levelLabel.gameObject.SetActive(!Active);
             Active = !Active;
-            //DontDestroyOnLoad(this.gameObject);
         }
         if (Input.GetKey(KeyCode.Minus)){
             SceneManager.LoadScene("DevRoom");
             levelLabel.text = "Dev Room 0";
+            player.position = targetPosition;
         }
         if(Input.GetKey(KeyCode.Alpha0)){
             SceneManager.LoadScene("Beach");
             levelLabel.text = "Beach ";
+            player.position = targetPosition;
         }
         if(Input.GetKey(KeyCode.Alpha1)){
             SceneManager.LoadScene("Castle_Exterior");
             levelLabel.text = "Outside Castle";
+            player.position = targetPosition;
         }
         if(Input.GetKey(KeyCode.Alpha2)){
             SceneManager.LoadScene("Castle_Interior");
             levelLabel.text = "Inside Castle";
+            player.position = targetPosition;
         }
         if(Input.GetKey(KeyCode.Alpha3)){
             SceneManager.LoadScene("Church_exterior");
             levelLabel.text = "Outside Church";
+            player.position = targetPosition;
         }
         if(Input.GetKey(KeyCode.Alpha4)){
             SceneManager.LoadScene("Church_Interior");
             levelLabel.text = "Inside Church";
+            player.position = targetPosition;
         }
         if(Input.GetKey(KeyCode.Alpha5)){
             SceneManager.LoadScene("Farm");
             levelLabel.text = "Farm";
+            player.position = targetPosition;
         }
         if(Input.GetKey(KeyCode.Alpha6)){
             SceneManager.LoadScene("Forest");
             levelLabel.text = "Forest";
+            player.position = targetPosition;
         }
         if(Input.GetKey(KeyCode.Alpha7)){
             SceneManager.LoadScene("Mine_Interior");
             levelLabel.text = "Mine";
+            player.position = targetPosition;
         }
         if(Input.GetKey(KeyCode.Alpha8)){
             SceneManager.LoadScene("Prison");
             levelLabel.text = "Prison";
+            player.position = targetPosition;
         }
         if(Input.GetKey(KeyCode.Alpha9)){
             SceneManager.LoadScene("Wizard_Exterior");
             levelLabel.text = "Outside Wizard Hut";
+            player.position = targetPosition;
         }
         if(Input.GetKey(KeyCode.Tilde)){
             SceneManager.LoadScene("Wizard_Interior");
             levelLabel.text = "Inside Wizard Hut";
+            player.position = targetPosition;
         }
     }
 }
