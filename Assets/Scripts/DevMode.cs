b662165f9a3d16ec4d4351863d61ef0101051964
GenWhiskers <tfrench2@harding.edu>
2022-10-12 21:41:16 -0500

    PLayer stuff

--- Assets/Scripts/DevMode.cs
+++ Assets/Scripts/DevMode.cs
@@ -1,51 +1,67 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 public class DevMode: MonoBehaviour
 {
     // Start is called before the first frame update
-
+    public Text levelLabel;
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Minus)){
+        if (Input.GetKey(KeyCode.D) && Input.GetKey(KeyCode.LeftControl))
+        {
+            
+        }
+        if (Input.GetKey(KeyCode.Minus)){
             SceneManager.LoadScene("DevRoom");
+            levelLabel.text = "Dev Room 0";
             //Debug.Log("Go to DevRoom");
         }
-        if(Input.GetKeyDown(KeyCode.Alpha0)){
+        if(Input.GetKey(KeyCode.Alpha0)){
             SceneManager.LoadScene("Beach");
+            levelLabel.text = "Beach ";
         }
-        if(Input.GetKeyDown(KeyCode.Alpha1)){
+        if(Input.GetKey(KeyCode.Alpha1)){
             SceneManager.LoadScene("Castle_Exterior");
+            levelLabel.text = "Outside Castle";
         }
-        if(Input.GetKeyDown(KeyCode.Alpha2)){
+        if(Input.GetKey(KeyCode.Alpha2)){
             SceneManager.LoadScene("Castle_Interior");
+            levelLabel.text = "Inside Castle";
         }
-        if(Input.GetKeyDown(KeyCode.Alpha3)){
+        if(Input.GetKey(KeyCode.Alpha3)){
             SceneManager.LoadScene("Church_exterior");
+            levelLabel.text = "Outside Church";
         }
-        if(Input.GetKeyDown(KeyCode.Alpha4)){
+        if(Input.GetKey(KeyCode.Alpha4)){
             SceneManager.LoadScene("Church_Interior");
+            levelLabel.text = "Inside Church";
         }
-        if(Input.GetKeyDown(KeyCode.Alpha5)){
+        if(Input.GetKey(KeyCode.Alpha5)){
             SceneManager.LoadScene("Farm");
+            levelLabel.text = "Farm";
         }
-        if(Input.GetKeyDown(KeyCode.Alpha6)){
+        if(Input.GetKey(KeyCode.Alpha6)){
             SceneManager.LoadScene("Forest");
+            levelLabel.text = "Forest";
         }
-        if(Input.GetKeyDown(KeyCode.Alpha7)){
+        if(Input.GetKey(KeyCode.Alpha7)){
             SceneManager.LoadScene("Mine_Interior");
+            levelLabel.text = "Mine";
         }
-        if(Input.GetKeyDown(KeyCode.Alpha8)){
+        if(Input.GetKey(KeyCode.Alpha8)){
             SceneManager.LoadScene("Prison");
+            levelLabel.text = "Prison";
         }
-        if(Input.GetKeyDown(KeyCode.Alpha9)){
+        if(Input.GetKey(KeyCode.Alpha9)){
             SceneManager.LoadScene("Wizard_Exterior");
+            levelLabel.text = "Outside Wizard Hut";
         }
-        if(Input.GetKeyDown(KeyCode.Tilde)){
+        if(Input.GetKey(KeyCode.Tilde)){
             SceneManager.LoadScene("Wizard_Interior");
+            levelLabel.text = "Inside Wizard Hut";
         }
     }
 }
