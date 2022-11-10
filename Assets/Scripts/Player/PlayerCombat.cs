4e3d31a2f55349a69989ce11a62d5f57907b0120
nbradleyCodes <112526708+nbradleyCodes@users.noreply.github.com>
2022-11-09 18:27:02 -0600

    attack is now left click

--- Assets/Scripts/Player/PlayerCombat.cs
+++ Assets/Scripts/Player/PlayerCombat.cs
@@ -27,7 +27,7 @@ public class PlayerCombat : MonoBehaviour
     {
         if (Time.time >= nextAttackTime)
         {
-            if (Input.GetKeyDown(KeyCode.Space))
+            if (Input.GetKeyDown(KeyCode.Mouse0))
             {
                 Attack();
                 nextAttackTime = Time.time + 1f / attackRate;
