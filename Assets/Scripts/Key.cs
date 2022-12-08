c2b70937114f7679e354b6481af13a2c3cb7e9da
nbradleyCodes <nbradley@harding.edu>
2022-12-08 01:35:14 -0600

    add items to inventory by pick

--- Assets/Scripts/Key.cs
+++ Assets/Scripts/Key.cs
@@ -13,18 +13,21 @@ public class Key : MonoBehaviour {
     //emorataya: Saving System test
     public string collectableId;
     public string sceneName;
+    public Transform pickUpSpot;
+    public float pickUpRange = 1f;
+    public LayerMask playerLayer;
 
-	// Use this for initialization
+    // Use this for initialization
     // Used for ingame text prompt
-	/*private void Start () {
+    /*private void Start () {
         pickUpText.gameObject.SetActive(false);
 	}*/
 
-	// Update is called once per frame
-	private void Update () {
+    // Update is called once per frame
+    private void Update () {
         //emorataya: Saving System test
         if(PlayerPrefs.GetString(collectableId) == "collected") {
-            PickUp();
+            //PickUp();
         }
 
         if (pickUpAllowed && Input.GetKeyDown(KeyCode.E))
@@ -51,11 +54,24 @@ public class Key : MonoBehaviour {
         }
     }
 
+    private void OnDrawGizmos()
+    {
+        if (pickUpSpot == null)
+        {
+            return;
+        }
+        Gizmos.DrawWireSphere(pickUpSpot.position, pickUpRange);
+    }
+
     private void PickUp()
     {
         //emorataya: Saving System test
         PlayerPrefs.SetString(collectableId, "collected");
-        Destroy(gameObject);
+        Collider2D player = Physics2D.OverlapCircle(pickUpSpot.position, pickUpRange, playerLayer);
+        GameObject item = this.gameObject;
+        player.GetComponent<Player>().PickUp(item);
+        Destroy(this.gameObject);
     }
 
 }
+
